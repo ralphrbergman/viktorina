d64cd806300765861fc2eaac4ae4ef300b8a1436
ralphrbergman <ralfs@esmuralfs.lv>
2026-05-01 23:55:00 +0300

    Added build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+NOT_IN_BUILD=false
+
+if [ "$(basename "$PWD")" != "build" ]; then
+    NOT_IN_BUILD=true
+
+    mkdir -p "build"
+    cd "build"
+fi
+
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release && make
+
+if [ "$NOT_IN_BUILD" = true ]; then
+    cd ..
+fi
