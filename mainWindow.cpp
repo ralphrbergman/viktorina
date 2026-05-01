22eac0627dbc2a79b1d077b7d545fbdab6ddd6c5
ralphrbergman <ralfs@esmuralfs.lv>
2026-05-02 00:30:00 +0300

    Added image loading and display

--- mainWindow.cpp
+++ mainWindow.cpp
@@ -1,5 +1,6 @@
 #include "mainWindow.h"
 #include "ui_questionaire.h"
+#include <filesystem>
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
@@ -9,6 +10,38 @@ MainWindow::MainWindow(QWidget *parent)
 , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    std::string cels = QCoreApplication::applicationDirPath().toStdString() + "/";
+    std::string faila_nosaukums;
+
+    // Ielādējam tematisko attēlu no diska un parādam to.
+    // Izvēloties starp PNG un JPEG attēliem.
+    if (std::filesystem::exists(cels + "attels.png")) {
+        faila_nosaukums = cels + "attels.png";
+    }
+    else if (std::filesystem::exists(cels + "attels.jpg")) {
+        faila_nosaukums = cels + "attels.jpg";
+    }
+    else if (std::filesystem::exists(cels + "attels.jpeg")) {
+        faila_nosaukums = cels + "attels.jpeg";
+    }
+    else {
+        // Ir pieņemami ka lietotne nenosprāgst dēļ neeksistējoša attēla tapēc vienkārši paziņojam.
+        qDebug() << "Tematiskais attēls nav atrodams, ignorējam.";
+    }
+
+    if (!faila_nosaukums.empty()) {
+        QPixmap attels = QPixmap(QString::fromStdString(faila_nosaukums)).scaled(
+            ui->attels->size(),
+            Qt::KeepAspectRatio,
+            Qt::SmoothTransformation
+        );
+
+        if (!attels.isNull()) {
+            ui->attels->setPixmap(attels);
+            ui->attels_2->setPixmap(attels);
+        }
+    }
 }
 
 MainWindow::~MainWindow() {
