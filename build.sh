f73c001d1bdca7ef9f4702c90c81f1477863397d
ralphrbergman <ralfs@esmuralfs.lv>
2026-05-02 15:58:30 +0300

    Added multi-core parameter to CMake

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ if [ "$(basename "$PWD")" != "build" ]; then
     cd "build"
 fi
 
-cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release && make
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build . -- -j$(nproc)
 
 if [ "$NOT_IN_BUILD" = true ]; then
     cd ..
